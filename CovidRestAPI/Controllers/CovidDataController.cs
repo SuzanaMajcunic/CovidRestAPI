9411c52eb7eb061bdf3b6069ac9a27eeda9933d9
suzanam <suzana.majcunic@in2.eu>
2020-12-07 21:49:14 +0100

    Add project files.

---
+++ CovidRestAPI/Controllers/CovidDataController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CovidRestAPI.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace CovidRestAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CovidDataController : ControllerBase
+    {
+        private readonly CovidDBContext _context;
+        static readonly HttpClient client = new HttpClient();
+
+        public CovidDataController(CovidDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CovidData
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CovidData>>> GetCovidData()
+        {
+            var result = await _context.CovidData.ToListAsync();
+
+            if (result != null && result.Count == 0)
+                return NotFound("No Covid data in database.");
+            return result;
+        }
+
+        // GET: CovidData/import
+        [HttpGet("import")]
+        public async Task<ActionResult<string>> GetCovidDataFromPublicAPI()
+        {
+            // Returns all cases by case type for a country from the first recorded case.
+            HttpResponseMessage response = await client.GetAsync("https://api.covid19api.com/dayone/country/croatia");
+            if (response.IsSuccessStatusCode)
+            {
+                List<CovidData> covidList;
+                var responseJson= await response.Content.ReadAsStringAsync();
+
+                try
+                {
+                    covidList = JsonConvert.DeserializeObject<List<CovidData>>(responseJson);
+                }
+                catch (Exception)
+                {
+                    return BadRequest("Error while data serialization.");
+                }
+
+                foreach (var covidData in covidList)
+                {
+                    if (!CovidDataExists(covidData.CountryCode, covidData.Date))
+                    {
+                        _context.CovidData.Add(covidData);
+                    }
+                }
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException)
+                {
+                    return BadRequest("Error while saving in DB.");
+                }
+
+                return Ok(responseJson);
+            }
+            return "Can not get response from api.covid19api.com. Status: " + response.StatusCode.ToString();
+        }
+
+        // GET: CovidData/byDate?from=a&to=b&minConfirmed=c&maxConfirmed=d
+        [HttpGet("byDate")]
+        public async Task<ActionResult<List<CovidData>>> GetCovidData(DateTime from, DateTime to, int? minConfirmed, int? maxConfirmed)
+        {
+            if (from == new DateTime()) return BadRequest("Parameter 'from' is obligatory.");
+            if (to == new DateTime()) return BadRequest("Parameter 'to' is obligatory.");
+            if (minConfirmed == null) return BadRequest("Parameter 'minConfirmed' is obligatory.");
+            else if (minConfirmed < 0) return BadRequest("Parameter 'minConfirmed' must be positive number.");
+            if (maxConfirmed == null) return BadRequest("Parameter 'maxConfirmed' is obligatory.");
+            else if (maxConfirmed < 0) return BadRequest("Parameter 'maxConfirmed' must be positive number.");
+
+            var covidData = await _context.CovidData.Where(x => x.Date >= from && x.Date <= to && x.Confirmed >= minConfirmed && x.Confirmed <= maxConfirmed).ToListAsync();
+
+            if (covidData.Count == 0)
+            {
+                return NotFound("No Covid data in database by given parameters.");
+            }
+
+            return covidData;
+        }
+
+        // GET: CovidData/single?countryCode=a&caseDate=b
+        [HttpGet("single")]
+        public async Task<ActionResult<CovidData>> GetSingleCovidData(string countryCode, DateTime caseDate)
+        {
+            if(string.IsNullOrEmpty(countryCode)) return BadRequest("Parameter 'countryCode' is obligatory.");
+            if (caseDate == new DateTime()) return BadRequest("Parameter 'caseDate' is obligatory.");
+
+            var covidData = await _context.CovidData.FirstOrDefaultAsync(x => x.CountryCode == countryCode && x.Date == caseDate);
+
+            if (covidData == null)
+            {
+                return NotFound("No Covid data in database by given parameters.");
+            }
+
+            return covidData;
+        }
+
+        // POST: CovidData
+        [HttpPost]
+        public async Task<ActionResult<CovidData>> PostCovidData(CovidData covidData)
+        {
+            _context.CovidData.Add(covidData);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CovidDataExists(covidData.CountryCode, covidData.Date))
+                {
+                    return Conflict("This Covid data is already contained in database.");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetCovidData", new { id = covidData.CountryCode }, covidData);
+        }
+
+        // DELETE: CovidData/countryCode=a&caseDate=b
+        [HttpDelete]
+        public async Task<ActionResult<CovidData>> DeleteCovidData(string countryCode, DateTime caseDate)
+        {
+            if (string.IsNullOrEmpty(countryCode)) return BadRequest("Parameter 'countryCode' is obligatory.");
+            if (caseDate == new DateTime()) return BadRequest("Parameter 'caseDate' is obligatory.");
+
+            var covidData = await _context.CovidData.FirstOrDefaultAsync(x => x.CountryCode == countryCode && x.Date == caseDate);
+            
+            if (covidData == null)
+            {
+                return NotFound("No Covid data in database by given parameters.");
+            }
+
+            _context.CovidData.Remove(covidData);
+            await _context.SaveChangesAsync();
+
+            return covidData;
+        }
+
+        private bool CovidDataExists(string countryCode, DateTime caseDate)
+        {
+            return _context.CovidData.Any(e => e.CountryCode == countryCode && e.Date == caseDate);
+        }
+    }
+}
---
+++ CovidRestAPI/Models/CovidDBContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CovidRestAPI.Models
+{
+    public partial class CovidDBContext : DbContext
+    {
+        public CovidDBContext(DbContextOptions<CovidDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<CovidData> CovidData { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CovidData>(entity =>
+            {
+                entity.HasKey(e => new { e.CountryCode, e.Date });
+
+                entity.ToTable("covidData");
+
+                entity.Property(e => e.CountryCode)
+                    .HasColumnName("countryCode")
+                    .HasMaxLength(3);
+
+                entity.Property(e => e.Date)
+                    .HasColumnName("caseDate")
+                    .HasColumnType("smalldatetime");
+
+                entity.Property(e => e.Active).HasColumnName("active");
+
+                entity.Property(e => e.City)
+                    .HasColumnName("city")
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.CityCode)
+                    .HasColumnName("cityCode")
+                    .HasMaxLength(12);
+
+                entity.Property(e => e.Confirmed).HasColumnName("confirmed");
+
+                entity.Property(e => e.Country)
+                    .IsRequired()
+                    .HasColumnName("country")
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Deaths).HasColumnName("deaths");
+
+                entity.Property(e => e.Lat)
+                    .HasColumnName("lat")
+                    .HasColumnType("decimal(9, 6)");
+
+                entity.Property(e => e.Lon)
+                    .HasColumnName("lon")
+                    .HasColumnType("decimal(9, 6)");
+
+                entity.Property(e => e.Province)
+                    .HasColumnName("province")
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Recovered).HasColumnName("recovered");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
